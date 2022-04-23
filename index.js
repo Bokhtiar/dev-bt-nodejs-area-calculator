71168bb710b3d24ae67abd013881d67f4dbc81b2
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-24 01:07:38 +0600

    circle and trainangle route and file create done

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+app.get("/circle", (req, res) => {
+    res.sendFile(__dirname + "/views/circle.html")
+});
+
+app.get("/traiangle", (req, res) => {
+    res.sendFile(__dirname + "/views/traiangle.html")
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`welcome ${PORT}`)
 })
+
