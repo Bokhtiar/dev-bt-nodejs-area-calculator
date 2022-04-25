c16324ffe421c529be54d3a23a39bcdc569291dd
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-25 11:49:38 +0600

    circle traiange page logical function done

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
@@ -12,6 +18,19 @@ app.get("/circle", (req, res) => {
 
 app.get("/traiangle", (req, res) => {
     res.sendFile(__dirname + "/views/traiangle.html")
+})
+
+app.post("/traiangle/store", (req, res) => {
+    const base  = res.body.base;
+    const height = res.body.height;
+    const area = 0.5 * base * height;
+    res.send(`area of traiangle is : ${area}`); 
+})
+
+app.post("/circle/store", (req, res) => {
+    const radius = res.body.base;
+    const area = Math.PI * radius * radius;
+    res.send(`circle is ${area}`)
 })
 
 
